39b0ce446f8037dfc0088fecaafec8208b2ac007
[s <basika.sumanth@gmsil.com>
2023-05-20 22:19:18 -0400

    changes made

--- First.java
+++ First.java
@@ -4,6 +4,12 @@ public class First {
         int a=2;
         int y=3;
         int c=a+y;
+        if (int a=0){
+            printf("display ");
+            else{
+                printf("not to display");
+        }
+    }
         System.out.println(c);
     }
 }
